be6c018b88423a7f7f0d500a7dccdb507b2c375b
iamareebjamal <jamal.areeb@gmail.com>
2017-02-07 21:40:02 +0530

    feat: Change Eclipse structure to Android Studio structure

--- app/src/main/java/areeb/scratch/iconpack/MainActivity.java
+++ app/src/main/java/areeb/scratch/iconpack/MainActivity.java
@@ -4,10 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
 }
